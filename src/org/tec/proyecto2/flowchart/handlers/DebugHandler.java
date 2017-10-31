9e530584b7b12920226ec31eb036f36a6665f574
Kisung <kisungqueso@hotmail.com>
2017-10-31 12:42:35 -0600

    Boton del debug
    Agregue el boton para iniciar la graficacion

---
+++ src/org/tec/proyecto2/flowchart/handlers/DebugHandler.java
@@ -0,0 +1,99 @@
+package org.tec.proyecto2.flowchart.handlers;
+
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+
+public class DebugHandler extends AbstractHandler {
+
+    private static final String JDT_NATURE = "org.eclipse.jdt.core.javanature";
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        IWorkspaceRoot root = workspace.getRoot();
+        // Get all projects in the workspace
+        IProject[] projects = root.getProjects();
+        // Loop over all projects
+        for (IProject project : projects) {
+            try {
+                if (project.isNatureEnabled(JDT_NATURE)) {
+                    analyseMethods(project);
+                }
+            } catch (CoreException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private void analyseMethods(IProject project) throws JavaModelException {
+        IPackageFragment[] packages = JavaCore.create(project)
+                .getPackageFragments();
+        // parse(JavaCore.create(project));
+        for (IPackageFragment mypackage : packages) {
+            if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+                createAST(mypackage);
+            }
+
+        }
+    }
+
+    private void createAST(IPackageFragment mypackage)
+            throws JavaModelException {
+        for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
+            // now create the AST for the ICompilationUnits
+            CompilationUnit parse = parse(unit);
+            MethodVisitor visitor = new MethodVisitor();
+            parse.accept(visitor);
+            
+            for (MethodDeclaration method : visitor.getMethods()) {
+            	List<?> names = method.getBody().statements();
+            	for (Object statement : names) {           		
+            		if (statement.toString().contains("if")) {
+            			System.out.println("if");
+            		} else if( statement.toString().contains("for")) {
+            			System.out.println("for");
+            		} else {
+            			System.out.println("");
+            		}
+            	}
+            }
+
+        }
+    }
+
+    /**
+     * Reads a ICompilationUnit and creates the AST DOM for manipulating the
+     * Java source file
+     *
+     * @param unit
+     * @return
+     */
+
+    @SuppressWarnings("deprecation")
+	private static CompilationUnit parse(ICompilationUnit unit) {
+        ASTParser parser = ASTParser.newParser(AST.JLS3);
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
+        parser.setSource(unit);
+        parser.setResolveBindings(true);
+        return (CompilationUnit) parser.createAST(null); // parse
+    }
+}
+
